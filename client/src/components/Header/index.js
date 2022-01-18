c8c0048b0d672601d98f0cdc1bd1cd24550012c2
ciisiq <cii_3@hotmail.com>
2022-01-18 15:47:53 +0000

    Header done

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div>
+      <img
+        width="60px"
+        alt="school of code"
+        src="https://d33wubrfki0l68.cloudfront.net/e6fddcbea146f91d2f3c160f7d56a9391a4740b0/4e758/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"
+      />
+      <h1>School of Code Mood Tracker</h1>
+    </div>
+  );
+}
+export default Header;
