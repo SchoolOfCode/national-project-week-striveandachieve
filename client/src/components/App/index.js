079d7fe5ddf5fcb7b4c7c67546fef891223939b6
HannahWhelan256 <hannahwhelan256@gmail.com>
2022-01-18 14:08:54 +0000

    added to react App component

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from "../Header";
+import Form from "../Form";
+import Results from "../Results";
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Header />
+      <Form />
+      <Results />
     </div>
   );
 }
