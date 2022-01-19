73b5995ff6a2b9699328ef32379109a1767db45a
HannahWhelan256 <hannahwhelan256@gmail.com>
2022-01-19 12:49:31 +0000

    removed .env files

--- db/index.js
+++ db/index.js
@@ -1,8 +1,7 @@
 import pg from 'pg';
-import * as config from "../config.js"
 
 const pool = new pg.Pool({
-connectionString: config.url,
+connectionString: process.env.DATABASE_URL,
 ssl: {rejectUnauthorized: false},
 });
 
