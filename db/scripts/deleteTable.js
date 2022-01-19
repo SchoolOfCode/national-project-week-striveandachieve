c8865345a35ed3a95fba4ca667e1685ea58a94f0
AleksApilats <shurikapilats@googlemail.com>
2022-01-19 22:14:51 +0000

    added consolelog to output when table is added populated or deleted

--- db/scripts/deleteTable.js
+++ db/scripts/deleteTable.js
@@ -1,9 +1,11 @@
 import query from "../index.js";
 
-const deleteTableString = "DELETE FROM dailymood";
+const deleteTableString = "DELETE FROM testdailymood";
 
 async function deleteTable() {
   const res = await query(deleteTableString);
+  //Added console output to show what table has been deleted
+  console.log("Table Deleted", res);
 }
 
 deleteTable();
--- db/scripts/populateTable.js
+++ db/scripts/populateTable.js
@@ -1,18 +1,23 @@
 import query from "../index.js";
 import { sampleData } from "../../app.js";
 
-async function populateMoodTable(){
-    for (let i=0; i<sampleData.length; i++){
-       const userid = sampleData[i].userid; 
-       const name = sampleData[i].name;
-       const week = sampleData[i].week;
-       const day = sampleData[i].day;
-       const learned = sampleData[i].learned;
-       const revise = sampleData[i].revise;
-       const mood = sampleData[i].mood;
-       const feeling = sampleData[i].feeling;
-       const res = await query(`INSERT INTO dailymood (userid, name, week, day, learned, revise, mood, feeling) VALUES($1,$2,$3,$4,$5,$6,$7,$8)`,[userid, name, week, day, learned, revise, mood, feeling]);
-        };
-        };
+async function populateMoodTable() {
+  for (let i = 0; i < sampleData.length; i++) {
+    const userid = sampleData[i].userid;
+    const name = sampleData[i].name;
+    const week = sampleData[i].week;
+    const day = sampleData[i].day;
+    const learned = sampleData[i].learned;
+    const revise = sampleData[i].revise;
+    const mood = sampleData[i].mood;
+    const feeling = sampleData[i].feeling;
+    const res = await query(
+      `INSERT INTO dailymood (userid, name, week, day, learned, revise, mood, feeling) VALUES($1,$2,$3,$4,$5,$6,$7,$8)`,
+      [userid, name, week, day, learned, revise, mood, feeling]
+    );
+    //Added console output of what has been added
+    console.log(res);
+  }
+}
 
 populateMoodTable();
