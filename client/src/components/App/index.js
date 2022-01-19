37134fff0b183909c4b784e4ed0c4472c9b0551d
Wesley Kelly <wesley.kelly@hotmail.com>
2022-01-19 09:36:58 +0000

    input functionality added using react hook form npm

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "../Header";
-import Form from "../Form";
+import Form2 from "../Form2";
 import Results from "../Results";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <h1>Hello World!</h1>
       {/* <Header /> */}
-      <Form />
+      <Form2 />
       {/* <Results /> */}
     </div>
   );
