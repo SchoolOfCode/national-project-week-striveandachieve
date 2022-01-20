e5eed785748d2a9b47dd8b9114a71f94717d0d1c
Wesley Kelly <wesley.kelly@hotmail.com>
2022-01-20 11:30:51 +0000

    Learner name and id added

---
+++ client/src/components/UserName/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./index.css"
+const UserNameField = () => {
+    return (
+        <div className='userNameBox'>
+         <h3>Name: Grace Covey</h3>  
+         <h3> ID: 20391234</h3>
+        </div>
+    )
+}
+
+export default UserNameField
