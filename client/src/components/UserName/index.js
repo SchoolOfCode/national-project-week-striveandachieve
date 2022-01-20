756908f057a7a70edd4915d7f0546f06baab2e29
Wesley Kelly <wesley.kelly@hotmail.com>
2022-01-20 12:11:42 +0000

    made name more responsive

--- client/src/components/UserName/index.js
+++ client/src/components/UserName/index.js
@@ -4,7 +4,7 @@ const UserNameField = () => {
     return (
         <div className='userNameBox'>
          <h3>Name: Grace Covey</h3>  
-         <h3> ID: 20391234</h3>
+     
         </div>
     )
 }
